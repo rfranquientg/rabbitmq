be92cb3339e28fb2fe2dcda270252029a67358df
Rolando Franqui <rolando.franquinadal@entegris.com>
2022-11-02 16:37:52 -0400

    Adding error and logging functionality to the send.py file

--- send.py
+++ send.py
@@ -1,4 +1,5 @@
-
+from sqlite3 import connect
+from sre_constants import SUCCESS
 import sys
 import pika
 import base64
@@ -7,15 +8,57 @@ from os import listdir, walk, replace
 from os.path import join,splitext, exists
 from time import sleep
 import shutil
-import ssl
+import logging
+
+
+
+logging.basicConfig(filename = 'script.log',level=logging.DEBUG,format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+root = logging.getLogger()
+root.setLevel(logging.DEBUG)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+root.addHandler(handler)
+logging.getLogger("pika").propagate = False
 
 sleepTime = 5
 inputPath = r"\\127.0.0.1\c$\LAB_SHARED_FOLDER"
+# inputPath = r"\\RABBITMQ\Users\Administrator\Desktop\lab_shared_folder"
 longTermStorageLocation = r"\\127.0.0.1\c$\long_term"
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
 
-channel.queue_declare(queue='hello')
+
+def createConnection():
+    connectionSuccessful = False
+    global connection
+    global channel
+    while connectionSuccessful == False:
+        logging.info("Initiating Connection to RabbitMQ Instance")
+        try:
+            connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+            channel = connection.channel()
+            channel.queue_declare(queue='hello')
+            connectionSuccessful = True
+            logging.info("Connection to RabbitMQ sucessful")
+                
+        except:
+            logging.error("Connection to RabbitMQ unsucesfull, retying in 5 seconds")
+            sleep(5)
+            
+
+def checkConnection():
+    global connect_open
+    try:
+        pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        logging.debug("Connection active")
+        connect_open = True
+    except:
+        connect_open = False
+        logging.error(f"Connection check failed, retrying in {sleepTime} seconds")
+        sleep(sleepTime)
+    return connect_open
+
+
 
 def getFiles(dir):
     files = []
@@ -31,6 +74,7 @@ def checkForFiles(dir):
         return files
     else:
         print(f"No new files will check in {sleepTime} seconds")
+        logging.info(f"No new files will check in {sleepTime} seconds")
         return None
 
 def getFileExtension(file):
@@ -63,33 +107,37 @@ def moveFile(dir):
         os.makedirs(target)
     # target = longTermStorageLocation + fileName
     shutil.move(dir,target)
-    print("Moved " + fileName + " to long term storage")
-
+    logging.info(f"Moved {fileName}' to long term storage")
+    
 
 def main(dir):
     while True:
-        # sendOrNo = input("Do you want to send the File?")
-        answer = input('Send Files???')
         files = checkForFiles(dir)
         if files is not None:
             for f in files:
-                encodedFile = encodeFiles(f)
-                fileName, extension = getFileExtension(f)
-                dataDict = createMesseage(encodedFile,fileName,extension)
-                moveFile(f)
-                channel.basic_publish(exchange='',
-                                    routing_key='hello',
-                                    body=dataDict)
-                print("[X] Sent ", dataDict)
-                
+                    if checkConnection() == True:
+                        encodedFile = encodeFiles(f)
+                        fileName, extension = getFileExtension(f)
+                        dataDict = createMesseage(encodedFile,fileName,extension)
+                        channel.basic_publish(exchange='',
+                                            routing_key='hello',
+                                            body=dataDict)
+                        moveFile(f)
+                        print("[X] Sent ", dataDict)
+                    else:
+                        createConnection()
         else:
+            sleep(5)
             continue
-    
+            
+            
+
+        
 
 if __name__ == '__main__':
     try:
+        createConnection()
         main(inputPath)
-        # main()
     except KeyboardInterrupt:
         print('Interrupted')
         try:
--- test.py
+++ test.py
@@ -1,24 +1,37 @@
-# from distutils import extension
-# import sys
-# import pika
-# import base64
-# import os
-# from os import listdir, walk, replace
-# from os.path import join,splitext, exists
-# from time import sleep
+from distutils import extension
+import sys
+import pika
+import base64
+import os
+from os import listdir, walk, replace
+from os.path import join,splitext, exists
+from time import sleep
 import ast
 import json
 from time import sleep
-
+import logging
 ### Testing the read of a config file
+sleepTime = 1
 
+import pika 
+import time
 
-f = open('config.json')
-  
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
 
-data = json.load(f)
-  
-print(data['person'])
-# Closing file
-sleep(10)
-f.close()
+def checkConnection():
+    sleep(sleepTime)
+    global connect_open
+    try:
+        pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+        logging.debug("Connection active")
+        connect_open = True
+    except:
+        connect_open = False
+        logging.error(f"Connection retrying in {sleepTime} seconds")
+        sleep(sleepTime)
+    return connect_open
+
+while True:
+    print(checkConnection())
