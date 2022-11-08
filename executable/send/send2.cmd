1c71cf85fa86913f56efd4a778f4a135aef3c58c
Rolando Franqui <rolando.franquinadal@entegris.com>
2022-11-08 16:16:46 -0500

    Restructuring Folder Structure

--- executable/send/send2.cmd
+++
@@ -1,6 +0,0 @@
-
-@echo off
-rem This script was created by Nuitka to execute 'send2.exe' with Python DLL being found.
-set PATH=c:\users\rfranqui\MINICO~1;%PATH%
-set PYTHONHOME=C:\Users\rfranqui\Miniconda3
-"%~dp0.\send2.exe" %*
